56325f6f0fc8af67069ea700500ef09fbf5990d3
Nuno Neto <73204727+NunoNeto77@users.noreply.github.com>
2021-11-09 17:43:19 +0000

    Update Vampire.java

--- The Monster Exercise (Game)/src/com/company/Vampire.java
+++ The Monster Exercise (Game)/src/com/company/Vampire.java
@@ -4,10 +4,9 @@ public class Vampire extends Monster{
 
     private final double BITING_CHANCE = 0.2; // 20% de possibilidade de morder
     private final int HEALING_RATE = 10;
-    MonsterType monsterType;
 
-    public Vampire(String name) {
-        super(name, MonsterType.VAMPIRE);
+    public Vampire() {
+        super(MonsterType.VAMPIRE);
     }
 
     @Override
@@ -15,25 +14,12 @@ public class Vampire extends Monster{
         if (Math.random() < BITING_CHANCE) {
             if (getHealth() + HEALING_RATE > 100) {
                 setHealth(100);
-                return this.monsterType.getHitPower();
+                return getHitPower();
             }
             setHealth(getHealth() + HEALING_RATE);
         }
-        return this.monsterType.getHitPower();
+       // return getHitPower();
+        return super.getHitPower();
     }
 
-    @Override
-    public void defend(int damage) {
-        setHealth(getHealth() - damage);
-
-        if (getHealth() < 0) {
-            setHealth(0);
-        }
-
-        getHealth();
-    }
-
-    public boolean isAlive() {
-        return getHealth() > 0;
-    }
 }
