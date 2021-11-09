b8846f9b63b3bf11f44976d4dc70ad7950b370fa
Nuno Neto <73204727+NunoNeto77@users.noreply.github.com>
2021-11-09 17:37:14 +0000

    Update Mummy.java

--- The Monster Exercise (Game)/src/com/company/Mummy.java
+++ The Monster Exercise (Game)/src/com/company/Mummy.java
@@ -4,10 +4,10 @@ public class Mummy extends Monster{
 
     private final int UNROLL_DAMAGE = 5;
     private int hits;
-    MonsterType monsterType;
 
-    public Mummy(String name) {
-       super(name, MonsterType.MUMMY);
+
+    public Mummy() {
+       super(MonsterType.MUMMY);
        this.hits = 0;
     }
 
@@ -20,53 +20,10 @@ public class Mummy extends Monster{
             hits = 0;
             return 0;
         }
-        return this.monsterType.getHitPower();
+      //  return getHitPower();
+        return super.getHitPower();
     }
 
-    @Override
-    public void defend(int damage) {
-        setHealth(getHealth() - damage);
 
-        if (getHealth() < 0) {
-            setHealth(0);
-        }
-
-        getHealth();
-    }
-
-    public boolean isAlive() {
-        return getHealth() > 0;
-    }
 }
 
-/* package com.company;
-
-public class Mummy extends Monster{
-    private final int UNROLL_DAMAGE = 5;
-    private int hits;
-    private String name = "Mummy";
-    private MonsterType monsterType;
-
-    public Mummy(MonsterType monsterType) {
-        super(MonsterType.MUMMY);
-        this.hits = 0;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    public int attack() {
-        hits++;
-
-        if (hits == 3) {
-            setHealth(getHealth() - UNROLL_DAMAGE);
-            hits=0;
-            return 0;
-        }
-        return super.attack();
-    }
-
-
-} */
