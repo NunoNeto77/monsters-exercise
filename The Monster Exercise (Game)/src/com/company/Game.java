859c83ef0dc4e166fd698f2c3d118c183ed581ea
Nuno Neto <73204727+NunoNeto77@users.noreply.github.com>
2021-11-10 18:28:52 +0000

    Update Game.java

--- The Monster Exercise (Game)/src/com/company/Game.java
+++ The Monster Exercise (Game)/src/com/company/Game.java
@@ -43,8 +43,8 @@ public class Game {
                 normalRound(player1, player2);
         }
 
-        removeDeadMonster(player1);
-        removeDeadMonster(player2);
+        player1.removeDeadMonster(player1);
+        player2.removeDeadMonster(player2);
 
         player1.printMonsters(player1.getMonsters());
         player2.printMonsters(player2.getMonsters());
@@ -133,20 +133,6 @@ public class Game {
 
     }
 
-    public Monster[] removeDeadMonster(Player player) {
-        Monster[] tempMonster = {};
-        for (int i = 0; i < player.getMonsters().length; i++) {
-            // [1, 2, 3,4] --> 2
-            if (player.getMonsters()[i].isAlive()) {
-                //       [1, 3, 4,0]
-                tempMonster = Arrays.copyOf(tempMonster, tempMonster.length + 1);
-                tempMonster[tempMonster.length - 1] = player.getMonsters()[i];
-
-            }
-        }
-        player.setMonsters(tempMonster);
-        return player.getMonsters();
-
-    }
+   
 }
 
